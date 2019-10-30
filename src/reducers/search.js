96abccd49272ef03320837c9a17858fd0b5a29c0
Nela <mute.agony@gmail.com>
2019-10-30 15:15:45 +0100

    fix: adjust reducers to action creators

--- src/reducers/search.js
+++ src/reducers/search.js
@@ -1,9 +1,33 @@
+const initialState = {
+  pending: false,
+  movies: [],
+  error: null
+};
+
 export const search = (state = "", action) => {
   switch (action.type) {
-    case "FETCH_MOVIES":
-      console.log(action.payload);
-      return state;
+    case "FETCH_MOVIES_SUCCESS":
+      return {
+        ...state,
+        pending: false,
+        movies: action.payload
+      };
+    case "FETCH_MOVIES_ERROR":
+      return {
+        ...state,
+        pending: false,
+        error: action.error
+      };
+    case "FETCH_MOVIES_PENDING":
+      return {
+        ...state,
+        pending: true
+      };
     default:
       return state;
   }
 };
+
+export const getMovies = state => state.movies;
+export const getMoviesPending = state => state.pending;
+export const getMoviesError = state => state.error;
